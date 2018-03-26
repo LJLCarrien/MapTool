6b7fff9218b39af91c1cda8e4f26300c9e791d59
LJLCarrien <377262335@qq.com>
2018-03-26 15:59:40 +0800

    Update 小地图雏形2

--- Assets/MapHelperEditorTools.cs
+++ Assets/MapHelperEditorTools.cs
@@ -13,97 +13,103 @@ public class MapHelperEditorTools : EditorWindow
         var tWin = CreateInstance<MapHelperEditorTools>();
         tWin.Show();
     }
+    void OnDestroy()
+    {
+        Clean();
+    }
+
+    private void Clean()
+    {
+        if (mCam != null)
+        {
+            GameObject.DestroyImmediate(mCam.gameObject);
+        }
+    }
+
     private int mMapId = 0;
     private Vector2 mScreenSize = new Vector2(1280, 720);
 
     private Camera mCam;
-    private int mCamHeight = 128;
     private int mCamXOffset = 0;
     private int mCamZOffset = 0;
     private int mCamSize = 18;
-
-    private RenderTexture mRenderTexture;
-    private Texture mCamTakeTexture;
-
-    private Texture mMapTexture;
-    private int mMapWidth = 50;
+    private int mMapWidth = 128;
     private int mMapHeight = 0;
 
     private Vector3 mModelPos;
     private Vector3 mNGUIPos;
     private Vector3 mUIPos;
 
-    private GameObject mShowUIGo;
+    private GameObject mShowUIGO;
+    private UITexture mShowMapTexture;
 
+    private GameObject mWorldGO;
 
     private string mResult;
 
 
     void OnGUI()
     {
-        EditorGUILayout.BeginVertical();
+        EditorGUILayout.BeginVertical("Box");
         mMapId = EditorGUILayout.IntField("场景ID：", mMapId);
         mScreenSize = EditorGUILayout.Vector3Field("屏幕分辨率:", mScreenSize);
+        EditorGUILayout.EndVertical();
 
-        mCamHeight = EditorGUILayout.IntField("相机高度H：", mCamHeight);
+        EditorGUILayout.BeginVertical("Box");
         mCamXOffset = EditorGUILayout.IntField("水平移动X：", mCamXOffset);
         mCamZOffset = EditorGUILayout.IntField("垂直移动Z：", mCamZOffset);
         mCamSize = EditorGUILayout.IntField("Size：", mCamSize);
+        mCam = EditorGUILayout.ObjectField("俯视模型摄像机：", mCam, typeof(Camera), true) as Camera;
+        mCam = UpdateCamera();
+        EditorGUILayout.EndVertical();
 
-        mCamTakeTexture = EditorGUILayout.ObjectField("摄像机俯拍：", mCamTakeTexture, typeof(Texture), true) as Texture;
-
-
+        EditorGUILayout.BeginVertical("Box");
         mMapWidth = EditorGUILayout.IntField("地图宽：", mMapWidth);
         mMapHeight = (int)(mMapWidth / (mScreenSize.x / mScreenSize.y));
-
         EditorGUILayout.LabelField("地图高：", mMapHeight.ToString());
-
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("地图预览：");
-        mMapTexture = EditorGUILayout.ObjectField(mMapTexture, typeof(Texture), true, GUILayout.Width(mMapWidth), GUILayout.Height(mMapHeight)) as Texture;
-        EditorGUILayout.EndHorizontal();
-
-
-        mModelPos = EditorGUILayout.Vector3Field("Unnity position:", mModelPos);
-        mNGUIPos = EditorGUILayout.Vector3Field("NGUI Transform position:", mNGUIPos);
-        mShowUIGo = EditorGUILayout.ObjectField("地图UI直观显示：", mShowUIGo, typeof(GameObject), true) as GameObject;
-
+        mWorldGO = EditorGUILayout.ObjectField("模型Go：", mWorldGO, typeof(GameObject), true) as GameObject;
+        mShowUIGO = EditorGUILayout.ObjectField("模型Go->地图Icon：", mShowUIGO, typeof(GameObject), true) as GameObject;
+        mShowMapTexture = EditorGUILayout.ObjectField("UI地图Texture：", mShowMapTexture, typeof(UITexture), true) as UITexture;
         if (GUILayout.Button("抓取", GUILayout.Width(60)))
         {
-            if (mCam == null)
-                mCam = GetCamera();
+
             if (mCam != null)
             {
-                MakeCameraImg(mCam, mMapWidth, mMapHeight);
-                GameObject.DestroyImmediate(mCam.gameObject);
+                CamToPng(mCam, mMapWidth, mMapHeight);
                 AssetDatabase.Refresh();
             }
         }
-
         EditorGUILayout.EndVertical();
 
-        k.v = mModelPos;
+        if (mWorldGO != null)
+        {
+            mModelPos = EditorGUILayout.Vector3Field("Unity Transform pos:", mWorldGO.transform.position);
+            //mNGUIPos = EditorGUILayout.Vector3Field("NGUI Transform pos:", mNGUIPos);
+            mUIPos = EditorGUILayout.Vector3Field("Map Transform pos:", mUIPos);
 
-        UnityWorldToNGUIWorld();
-        if (mShowUIGo != null)
-            mShowUIGo.transform.position = new Vector3(mNGUIPos.x, mNGUIPos.y, 0);
+            UpdatUIInfo();
+        }
 
-        Vector2 unitOffset = CalOffset();
-        mResult = string.Format(" 缩放{0},偏移X:{1},偏移Y:{2}", mScreenSize.x / mMapWidth, unitOffset.x.ToString("f2"), unitOffset.y.ToString("f2"));
+
+
         EditorGUILayout.SelectableLabel(mResult);
+
+
     }
-
-    #region 计算
-
-    private void UnityWorldToNGUIWorld()
+    private void UpdatUIInfo()
     {
-        //unity world 2 Screen
-        Vector3 sPos = Camera.main.WorldToScreenPoint(mModelPos);
-        //unity Screen 2 NGUI world
-        if (mUICamera == null) return;
-        mNGUIPos = mUICamera.ScreenToWorldPoint(sPos);
-    }
 
+        mNGUIPos = UnityWorldToNGUIWorld(mModelPos);
+        mUIPos = WorldPosToMiniMapPos(mModelPos);
+        if (mShowUIGO != null)
+            mShowUIGO.transform.position = mUIPos;
+
+        mScale = mScreenSize.x / mMapWidth;
+        mUnitOffset = CalOffset();
+
+        mResult = string.Format(" 屏幕/地图比：{0},偏移X：{1},偏移Y：{2}\n图片保存路径：{3}", mScale.ToString(), mUnitOffset.x.ToString("f2"), mUnitOffset.y.ToString("f2"), savePath);
+
+    }
     Camera mUICamera;
     private void OnEnable()
     {
@@ -111,34 +117,34 @@ public class MapHelperEditorTools : EditorWindow
         if (cam != null) mUICamera = cam.cachedCamera;
     }
 
-    private Vector2 CalOffset()
+    #region 计算
+    private float mScale;//缩放
+    private Vector2 mUnitOffset;//偏移
+
+    private Vector3 CalOffset()
+    {
+        return UnityWorldToNGUIWorld(new Vector3(1, 0, 1));
+    }
+    private Vector3 UnityWorldToNGUIWorld(Vector3 worldPos)
     {
         //unity world 2 Screen
-        Vector3 sPos = Camera.main.WorldToScreenPoint(new Vector3(1, 0, 1));
+        Vector3 sPos = Camera.main.WorldToScreenPoint(worldPos);
         //unity Screen 2 NGUI world
-        if (mUICamera == null) return Vector2.zero;
+        if (mUICamera == null) return Vector3.zero;
         return mUICamera.ScreenToWorldPoint(sPos);
     }
-    private float _scale = 72 / 10;
-    private float mapOffsetX = 639f;
-    private float mapOffsetY = 358f;
-    private Vector3 ConverWorldPosToMinMapPos(float worldX, float worldZ)
-    {
-        return new Vector3(worldX * _scale + mapOffsetX, worldZ * _scale - mapOffsetY, 0);
-    }
-    private Transform m_Transform;
-    private float mRadius = 1;
-    public Color mColor = Color.green;
-    public float m_Theta = 0.1f;
 
-    private void DrawPos()
+    private Vector3 WorldPosToMiniMapPos(Vector3 worldPos)
     {
-
+        Vector3 screenPos = UnityWorldToNGUIWorld(worldPos);
+        return new Vector3(screenPos.x / mScale, screenPos.y / mScale, 0);
     }
+
     #endregion
 
     #region  获取
-    private void MakeCameraImg(Camera mCam, int width, int height)
+    private string savePath;
+    private void CamToPng(Camera mCam, int width, int height)
     {
         RenderTexture rt = new RenderTexture(width, height, 2);
         mCam.targetTexture = rt;
@@ -149,16 +155,15 @@ public class MapHelperEditorTools : EditorWindow
         mCam.targetTexture = null;
         RenderTexture.active = null;
         UnityEngine.Object.DestroyImmediate(rt);
-
         SaveTextureToPng(screenShot, Application.dataPath + "/temp", GetMapName());
     }
 
     public void SaveTextureToPng(Texture2D png, string path, string pngName)
     {
-
         byte[] bytes = png.EncodeToPNG();
         if (!Directory.Exists(path))
             Directory.CreateDirectory(path);
+        savePath = path + "/" + pngName + ".png";
         FileStream file = File.Open(path + "/" + pngName + ".png", FileMode.Create);
         BinaryWriter writer = new BinaryWriter(file);
         writer.Write(bytes);
@@ -166,45 +171,12 @@ public class MapHelperEditorTools : EditorWindow
         Texture2D.DestroyImmediate(png);
     }
 
-    public Texture2D GetTexture2D(RenderTexture renderT)
-    {
-        if (renderT == null)
-            return null;
-
-        int width = renderT.width;
-        int height = renderT.height;
-        Texture2D tex2D = new Texture2D(width, height, TextureFormat.ARGB32, false);
-        RenderTexture.active = renderT;
-        tex2D.ReadPixels(new Rect(0, 0, width, height), 0, 0);
-        tex2D.Apply();
-        return tex2D;
-    }
-
-    //public Material GetMaterial()
-    //{
-    //    if (mMaterial == null)
-    //    {
-    //        //mMaterial = new Material("Shader \"MapHelperEditorTools / ForMapSharder\"" +
-    //        //                         " {Properties{_MainTex (\"Texture\", 2D) = \"white\" {}}" +
-    //        //    " SubShader{Tags { \"RenderType\" = \"Opaque\" }LOD 100" +
-    //        //    "Pass{CGPROGRAM #pragma vertex vert #pragma fragment frag # include \"UnityCG.cginc\"" +
-    //        //    "  struct appdata{float4 vertex : POSITION;float2 uv : TEXCOORD0;};" +
-    //        //    "struct v2f{float2 uv : TEXCOORD0;float4 vertex : SV_POSITION;};" +
-    //        //    " sampler2D _MainTex;float4 _MainTex_ST;" +
-    //        //    " v2f vert(appdata v){v2f o;o.vertex = UnityObjectToClipPos(v.vertex);o.uv = TRANSFORM_TEX(v.uv, _MainTex);return o;}" +
-    //        //    "fixed4 frag(v2f i) : SV_Target{fixed4 col = tex2D(_MainTex, i.uv);return col;}ENDCG}}}");
-
-    //        mMaterial = new Material(Shader.Find("MapHelperEditorTools/ForMapSharder"));
-    //    }
-    //    return mMaterial;
-    //}
-
     public string GetMapName()
     {
         return mMapId + "_" + mMapWidth + "x" + mMapHeight;
     }
 
-    public Camera GetCamera()
+    public Camera UpdateCamera()
     {
         if (mCam == null)
         {
@@ -214,6 +186,13 @@ public class MapHelperEditorTools : EditorWindow
             mCam.transform.eulerAngles = new Vector3(90f, 0f, 0f);
             mCam.transform.localScale = Vector3.one;
             mCam.orthographic = true;
+            mCam.orthographicSize = mCamSize;
+
+        }
+        else
+        {
+            mCam.transform.localPosition = new Vector3(mCamXOffset, 50, mCamZOffset);
+
             mCam.orthographicSize = mCamSize;
         }
         return mCam;
